7a9ede508c69cf04298a9adb7e22099ffea97ff9
haobin <haobin@haobindeMacBook-Pro.local>
2020-01-06 17:40:32 +0800

    hrs-cli

---
+++ README.md
@@ -0,0 +1,17 @@
+## react-typescript 项目的脚手架工具
+##### 集成了react-typescript项目使用的所有基本配置以及最合适的插件：
+```
+antd/antd-mobile UI框架
+axios HTTP库
+immutability-helper 更好的操作不可变数据的插件，例如redux修改state，修改深层嵌套的数据时
+reselect redux的中间件，缓存redux的数据，减少计算和渲染压力
+```
+### 1.全局下载脚手架工具
+```
+// 全局安装脚手架工具
+npm i hrts-cli -g
+```
+### 2.初始化项目
+```
+hrts-cli init [项目名称]
+```
---
+++ index.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const program = require('commander');
+const download = require('download-git-repo');
+const handlebars = require('handlebars');
+const inquirer = require('inquirer');
+const ora = require('ora');
+const chalk = require('chalk');
+const symbols = require('log-symbols');
+program.version('0.1.7', '-v, --version')
+  .command('init <name>')
+  .action((name) => {
+    if (!fs.existsSync(name)) {
+      inquirer.prompt([
+        {
+          name: 'appName',
+          message: '请输入项目名称',
+          default: 'hrts-app'
+        },
+        {
+          name: 'description',
+          message: '请输入项目描述',
+          default: 'react-typescript项目'
+        },
+        {
+          name: 'author',
+          message: '请输入作者名称'
+        }
+      ]).then((answers) => {
+        const spinner = ora('正在初始化项目...');
+        spinner.start();
+        download('https://github.com:Hbinbin/react-typescript-app-template#master', name, { clone: true }, (err) => {
+          if (err) {
+            spinner.fail();
+            console.log(symbols.error, chalk.red(err));
+          } else {
+            spinner.succeed();
+            const fileName = `${name}/package.json`;
+            const { appName, description, author } = answers
+            const meta = {
+              name: appName,
+              description,
+              author
+            }
+            if (fs.existsSync(fileName)) {
+              const content = fs.readFileSync(fileName).toString();
+              const result = handlebars.compile(content)(meta);
+              fs.writeFileSync(fileName, result);
+            }
+            console.log(symbols.success, chalk.green('项目初始化完成'));
+          }
+        })
+      })
+    } else {
+      // 错误提示项目已存在，避免覆盖原有项目
+      console.log(symbols.error, chalk.red('项目已存在'));
+    }
+  })
+program.parse(process.argv);
